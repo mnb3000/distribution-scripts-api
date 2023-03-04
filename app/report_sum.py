4c2d8cc2a924ad52c861e1bd61335354c5c5c4c9
mnb3000 <misha.mykolyshyn@gmail.com>
2023-03-04 05:10:53 +0200

    Changed sum data orientation

--- app/report_sum.py
+++ app/report_sum.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import pandas as pd
 
@@ -7,4 +8,6 @@ logger = logging.getLogger(__name__)
 def report_sum(file, artist_column="Artist", net_revenue_column="Net Revenue in USD", encoding="utf-8"):
     df = pd.read_csv(file, encoding=encoding)
     sums = df.groupby(artist_column).agg({net_revenue_column: 'sum'})
-    return sums.to_dict()[net_revenue_column]
+    jsonData = sums.to_json(orient='table')
+    parsed = json.loads(jsonData)
+    return parsed
